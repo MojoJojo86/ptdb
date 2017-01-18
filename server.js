b030f0e017b33063d639402ad3d9080476352c7a
Joe Bradshaw <itsgoneretro@googlemail.com>
2017-01-18 22:17:20 +0000

    boom

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(cors());
 
-app.use(express.static('public'));
+
 
 //app.get('/', function(req, res) {
   //res.sendFile(__direname + '/index.html')
