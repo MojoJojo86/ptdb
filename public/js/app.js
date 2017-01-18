65ae3a48c37a3272d17802d5dd5d6ec328c8c072
Joe Bradshaw <itsgoneretro@googlemail.com>
2017-01-18 21:32:02 +0000

    AGAIN!

--- public/js/app.js
+++ public/js/app.js
@@ -5,13 +5,13 @@
 
     app.controller('patientsController', function($scope, $http) {
 
-      $http.get('/')
+      $http.get('api/patients')
         .then(function(response) {
           $scope.patients = response.data;
         });
     
       $scope.savePatient = function(patient) {
-        $http.post('/patients', patient)
+        $http.post('api/patients', patient)
           .then(function(response) {
             $scope.patients.push(response.data);
         });
