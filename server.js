4f2d5e05bfa8d6ca6d88766b63f7dc6303130290
MojoJojo86 <itsgoneretro@googlemail.com>
2017-01-19 12:44:23 +0000

    Update server.js

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(cors());
 
-
+app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-  res.sendFile('public/index.html')
+  res.sendFile(__dirname + '/index.html')
 });
 
 app.use('/api/patients', require('./api/patients/routes/post_patient'));
