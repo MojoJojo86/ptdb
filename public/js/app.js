02caa45e84d8520951663ff517b542d91cd6d362
MojoJojo86 <itsgoneretro@googlemail.com>
2017-01-19 12:55:03 +0000

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -5,13 +5,13 @@
 
     app.controller('patientsController', function($scope, $http) {
 
-      $http.get('https://murmuring-ocean-78954.herokuapp.com/api/patients')
+      $http.get('/api/patients')
         .then(function(response) {
           $scope.patients = response.data;
         });
     
       $scope.savePatient = function(patient) {
-        $http.post('https://dashboard.heroku.com/apps/murmuring-ocean-78954/api/patients', patient)
+        $http.post('https://murmuring-ocean-78954.herokuapp.com/api/patients', patient)
           .then(function(response) {
             $scope.patients.push(response.data);
         });
