a79a841fc6e7185b64050383bb3dbb3802ce2606
MojoJojo86 <itsgoneretro@googlemail.com>
2017-01-19 11:41:35 +0000

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -5,13 +5,13 @@
 
     app.controller('patientsController', function($scope, $http) {
 
-      $http.get('api/patients')
+      $http.get('/api/patients')
         .then(function(response) {
           $scope.patients = response.data;
         });
     
       $scope.savePatient = function(patient) {
-        $http.post('api/patients', patient)
+        $http.post('/api/patients', patient)
           .then(function(response) {
             $scope.patients.push(response.data);
         });
